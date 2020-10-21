fe47b2070d79d8920dcec3a3fcc1efd8d160a9de
Bence Papp <pappbence96@gmail.com>
2020-10-21 13:04:55 +0200

    Added combat ability to UnitsService

--- backend/StrategyGame.Bll/Services/Units/IUnitsService.cs
+++ backend/StrategyGame.Bll/Services/Units/IUnitsService.cs
@@ -6,6 +6,7 @@ namespace StrategyGame.Bll.Services.Units
 {
     public interface IUnitsService
     {
+        Task<Combat> AttackAnotherCountry(int attackerId, int defenderId, Dictionary<int, int> attackingUnits);
         Task<IEnumerable<CountryUnitConnector>> GetAllUnitsOfCountry(int countryId);
         Task<IEnumerable<Unit>> GetAllUnitTypes();
         Task<IEnumerable<CountryUnitConnector>> RecruitUnitsForCountry(Dictionary<int, int> unitCounts, int countryId);
--- backend/StrategyGame.Bll/Services/Units/UnitsService.cs
+++ backend/StrategyGame.Bll/Services/Units/UnitsService.cs
@@ -60,5 +60,53 @@ namespace StrategyGame.Bll.Services.Units
             await context.SaveChangesAsync();
             return country.Units;
         }
+
+        public async Task<Combat> AttackAnotherCountry(int attackerId, int defenderId, Dictionary<int, int> attackingUnits)
+        {
+            // Validate whether both countries exist
+            var attacker = (await context.Countries.Include(c => c.Units).FirstOrDefaultAsync(r => r.Id == attackerId))
+                ?? throw new KeyNotFoundException($"Country with ID {attackerId} not found.");
+            var defender = (await context.Countries.FirstOrDefaultAsync(r => r.Id == defenderId))
+                ?? throw new KeyNotFoundException($"Country with ID {defenderId} not found.");
+
+            var combat = new Combat
+            {
+                Attacker = attacker,
+                Defender = defender,
+                Round = await context.Rounds.FirstAsync(r => r.IsActive)
+            };
+
+            // Construct attacking army (we can skip straight over those with 0 requested count)
+            foreach (var (unitId, unitCount) in attackingUnits.Where(u => u.Value > 0)) 
+            {
+                var unit = (await context.Units.FirstOrDefaultAsync(u => u.Id == unitId))
+                    ?? throw new KeyNotFoundException($"Unit with ID {unitId} not found.");
+
+                // Check if the attacker has enough free army of this type
+                int availableUnitCount = 0;
+                var attackerUnitOfType = attacker.Units.FirstOrDefault(u => u.Unit == unit);
+                if (attackerUnitOfType != null)
+                {
+                    availableUnitCount = attackerUnitOfType.TotalCount - attackerUnitOfType.InCombat;
+                }
+                if(unitCount > availableUnitCount)
+                {
+                    throw new InvalidOperationException($"Insufficient units in garrison (requested: {unitCount}, in garrison: {availableUnitCount}).");
+                }
+
+                // Add selected units to the army and mark them as "away in combat"
+                combat.Units.Add(new CombatUnitConnector
+                {
+                    Count = unitCount,
+                    Unit = unit
+                });
+                attackerUnitOfType.InCombat += unitCount;
+            }
+
+            await context.SaveChangesAsync();
+            return combat;
+        }
+
+
     }
 }
