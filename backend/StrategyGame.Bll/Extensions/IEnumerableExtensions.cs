50b7ef9a8213561b1394a78277d3a02e0b394d8d
Bence Papp <pappbence96@gmail.com>
2020-10-21 10:21:14 +0200

    Base for game tick logic, in need of refactor

---
+++ backend/StrategyGame.Bll/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrategyGame.Bll.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Computes the product of the sequence of System.Double values that are obtained by invoking a transform function on each element of the input sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="source">A sequence of values that are used to calculate a sum.</param>
+        /// <param name="selector">A transform function to apply to each element.</param>
+        /// <returns> The product of the projected values.</returns>
+        public static double Product<TSource>(this IEnumerable<TSource> source, Func<TSource, double> selector)
+        {
+            return source.Select(selector).Aggregate(1d, (acc, next) => acc * next);
+        }
+
+        
+        /// <summary>
+        /// Computes the product of the sequence of System.Int32 values that are obtained by invoking a transform function on each element of the input sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="source">A sequence of values that are used to calculate a sum.</param>
+        /// <param name="selector">A transform function to apply to each element.</param>
+        /// <returns> The product of the projected values.</returns>
+        public static int Product<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector)
+        {
+            return source.Select(selector).Aggregate(1, (acc, next) => acc * next);
+        }
+
+        
+        /// <summary>
+        /// Computes the product of the sequence of System.Float values that are obtained by invoking a transform function on each element of the input sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="source">A sequence of values that are used to calculate a sum.</param>
+        /// <param name="selector">A transform function to apply to each element.</param>
+        /// <returns> The product of the projected values.</returns>
+        public static float Product<TSource>(this IEnumerable<TSource> source, Func<TSource, float> selector)
+        {
+            return source.Select(selector).Aggregate(1f, (acc, next) => acc * next);
+        }
+
+
+    }
+}
---
+++ backend/StrategyGame.Bll/Services/GameEngineService/GameEngineService.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StrategyGame.Bll.Extensions;
+using StrategyGame.Dal;
+using StrategyGame.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyGame.Bll.Services.GameEngineService
+{
+    public class GameEngineService
+    {
+        private readonly UnderseaDbContext context;
+
+        public GameEngineService(UnderseaDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task PerformTick()
+        {
+            /* Egy körben megvalósítandó feladatok (ilyen sorrendben!):
+            •	adó jóváírása
+            •	korall jóváírása
+            •	zsold kifizetése
+            •	katonák etetése (a lakosokat nem kell, ők a sajátjukon felül termelik a megadott mennyiséget)
+            •	fejlesztés: minden fejlesztés 15 kört vesz igénybe, egyszerre csak egy dolog fejleszthető és minden csak egyszer fejleszthető ki
+            •	építkezés: minden épület 5 kör alatt épül fel, de egyszerre csak egy épülhet, ezekből természetesen lehet sok
+            •	harc
+            •	ranglétra helyezés számítása */
+
+
+            Round currentRound = await context.Rounds.SingleAsync(r => r.IsActive);
+
+            // Tax collection: pearls increase by (tax + buildingIncome) * researchTaxModifier
+            foreach(var country in context.Countries)
+            {
+                int tax = country.Population * 25;
+
+                int buildingProduction = country.Buildings
+                    .Where(c => c.IsComplete)
+                    .Sum(b => b.Building.PearlPerRound);
+
+                double researchModifier = country.Researches
+                    .Where(c => c.IsComplete)
+                    .Product(c => c.Research.TaxMultiplier);
+
+                double increment = (tax + buildingProduction) * researchModifier;
+                country.Pearl += (int)increment;
+            }
+
+            // Coral colletion: coral increse by buildingProduction * researchCoralModifierforeach(var country in context.Countries)
+            foreach(var country in context.Countries)
+            {
+                int buildingProduction = country.Buildings
+                    .Where(c => c.IsComplete)
+                    .Sum(b => b.Building.CoralPerRound);
+
+                double researchModifier = country.Researches
+                    .Where(c => c.IsComplete)
+                    .Product(c => c.Research.CoralMultiplier);
+
+                double increment = buildingProduction * researchModifier;
+                country.Coral += (int)increment;
+            }
+            
+            // Soldier pay (for now let's assume that countries can go into debt)
+            foreach(var country in context.Countries)
+            {
+                var totalPay = country.Units.Sum(c => c.TotalCount * c.Unit.Pay);
+                country.Pearl -= totalPay;
+            }
+
+            // Soldier supplying (for now let's assume that countries can go into debt)
+            foreach (var country in context.Countries)
+            {
+                var totalSupply = country.Units.Sum(c => c.TotalCount * c.Unit.Supply);
+                country.Coral -= totalSupply;
+            }
+
+            // Research progress
+            foreach(var research in context.CountryResearchConnectors.Where(c => !c.IsComplete))
+            {
+                research.RoundsLeftUntilCompletion--;
+            }
+
+            // Building progress
+            foreach(var building in context.CountryBuildingConnectors.Where(c => !c.IsComplete))
+            {
+                building.RoundsLeftUntilCompletion--;
+                if (building.IsComplete)
+                {
+                    building.Country.Population+= building.Building.PopulationOnConstructionFinished;
+                    building.Country.Garrison += building.Building.GarrisonOnConstructionFinished;
+                }
+            }
+
+            // Combat
+            foreach(var combat in currentRound.ActiveCombats)
+            {
+                // Attacking army strength
+                int attackerBaseStrength = combat.Units.Sum(u => u.Count * u.Unit.Attack);
+                double attackerAttackMultiplier = combat.Attacker.Researches.Where(r => r.IsComplete).Product(r => r.Research.AttackMultiplier);
+                double morale = (100 + new Random().Next(-5, 6)) / 100d; // morale between 95% and 105% 
+                double attackPower = attackerBaseStrength * attackerAttackMultiplier * morale;
+
+                // Defending army strength
+                int defenderBaseStrength = combat.Defender.Units.Sum(u => (u.TotalCount - u.InCombat) * u.Unit.Defense);
+                double defenderDefenseMultiplier = combat.Defender.Researches.Where(r => r.IsComplete).Product(r => r.Research.DefenseMultiplier);
+                double defensePower = defenderBaseStrength * defenderDefenseMultiplier;
+
+                // If the attacking army won
+                if(attackPower > defensePower)
+                {
+                    // Half of defending army dead, remove half of the pearl and coral amount
+                    foreach(var unit in combat.Defender.Units)
+                    {
+                        // Number of defenders is the amount of units minus those away in combat
+                        int deadUnits = (unit.TotalCount - unit.InCombat) / 2;
+                        unit.TotalCount -= deadUnits;
+                    }
+                    if(combat.Defender.Coral > 0)
+                    {
+                        int stolenCoral = combat.Defender.Coral / 2;
+                        combat.Defender.Coral -= stolenCoral;
+                        combat.Attacker.Coral += stolenCoral;
+                    }
+                    if(combat.Defender.Pearl > 0)
+                    {
+                        int stolenPearl = combat.Defender.Pearl / 2;
+                        combat.Defender.Pearl -= stolenPearl;
+                        combat.Attacker.Pearl += stolenPearl;
+                    }
+                }
+                else
+                {
+                    // Half of attacking army dead
+                    foreach(var attackerUnit in combat.Units)
+                    {
+                        int dead = attackerUnit.Count / 2;
+                        var totalUnitsOfType = combat.Attacker.Units.Single(u => u.Unit == attackerUnit.Unit);
+                        // Return the attacking troops home, except for the dead
+                        totalUnitsOfType.TotalCount -= dead;
+                    }
+                }
+                combat.IsConcluded = true;
+            }
+
+            // Reset the "units in combat" to 0 only after all the combats of the round were finished
+            foreach(var country in context.Countries)
+            {
+                foreach(var unit in country.Units)
+                {
+                    unit.InCombat = 0;
+                }
+            }
+
+            var newScoreboardEntries = new List<ScoreboardEntry>();
+            // Scoreboard calculation
+            foreach(var country in context.Countries)
+            {
+                var newEntry = new ScoreboardEntry
+                {
+                    Country = country,
+                    Round = currentRound,
+                    PopulationScore = country.Population * 1,
+                    BuildingScore = country.Buildings.Count(b => b.IsComplete) * 50,
+                    ArmyScore = country.Units.Sum(u => u.TotalCount * u.Unit.ScoreboardValue),
+                    ResearchScore = country.Researches.Count(r => r.IsComplete) * 100
+                };
+            }
+            // Order and set computed position
+            newScoreboardEntries = newScoreboardEntries.OrderByDescending(e => e.TotalScore).ToList();
+            for(int i = 0; i < newScoreboardEntries.Count; i++)
+            {
+                newScoreboardEntries[i].Position = i;
+            }
+
+            // Finalize round and start the next one
+            currentRound.IsActive = false;
+            currentRound.TickedAt = DateTime.UtcNow;
+            var newRound = new Round() { IsActive = true };
+            context.Rounds.Add(newRound);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
